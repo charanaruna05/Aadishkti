0f683d048fd0d233c39a77c59bbcfd710ca8f9e7
charanaruna05 <charanaruna051@gmail.com>
2026-04-18 20:13:13 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Box from "../components/Box";
+
+export default function Home() {
+return ( <div className="page">
+<h3 style={{textAlign:"center"}}>🔱 TRINETR 🔱</h3>
+
+```
+  <div className="grid">
+    <Box title="NIFTY" />
+    <Box title="SENSEX" />
+    <Box title="GOLD" />
+    <Box title="PROFILE" />
+    <Box title="BANKING" />
+    <Box title="IT" />
+  </div>
+</div>
+```
+
+);
+}
